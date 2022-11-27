5a4ade1befb1319bfa9406734fbaf1993d34995b
NgocTrongT <hoaganheo@gmail.com>
2022-11-27 16:40:22 +0700

    ngoctrong commit lich su giao dich

---
+++ app/src/main/java/com/example/drinkandcake/Lich_Su_Giao_Dich.java
@@ -0,0 +1,14 @@
+package com.example.drinkandcake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lich_Su_Giao_Dich extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lich_su_giao_dich);
+    }
+}
